dd44ae44cb4173a5d4cee43326976735b85cf5a8
Muhammad Arief <azkklk@gmail.com>
2024-08-02 11:22:20 +0800

    feat: Add verifycode component and showing test score

---
+++ src/app/api/submit-test/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@lib/prisma";
+import { auth } from "@auth";
+
+export async function POST(req: Request) {
+  const body = await req.json()
+  const { cid, courseId, userId } = body
+  const session = await auth()
+
+  if (!session) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { cid: session.user.cid },
+      select: { id: true }
+    })
+
+    if (user) {
+      const code = await prisma.verifyCode.create({
+        data: {
+          cid,
+          status: 'TEST PASSED',
+          courseId
+        },
+        select: { id: true }
+      })
+
+      await prisma.userCourse.update({
+        where: { userId_courseId: { userId: user.id, courseId: courseId } },
+        data: { completed: true }
+      })
+
+      if (code) return NextResponse.json({ message: 'Test submitted successfully', code: code.id }, { status: 200 });
+      else throw new Error('Failed to submit test');
+    }
+
+  } catch (e: any) {
+    return NextResponse.json({ message: e.message }, { status: 500 });
+  }
+}
--- src/components/pretest.tsx
+++ src/components/pretest.tsx
@@ -2,10 +2,13 @@
 import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { courses_data, Question } from '@data/constant-data'
+import axios from 'axios'
 
 interface PretestProps {
   id: number
+  setIsTestCompleted?: (value: boolean) => void
   questions?: Question[]
+  courseId?: string
 }
 
 interface ConfirmPretestProps extends PretestProps {
@@ -16,18 +19,46 @@ interface ConfirmPretestProps extends PretestProps {
 interface PretestQuestionProps extends PretestProps {
   currentQuestion: number
   setCurrentQuestion: (value: (prev: number) => number) => void
-  setScore?: (value: number) => void
+  setScore: (value: (prev: number) => number) => void
+  score: number
+  setLoading: (value: boolean) => void
 }
 
-const Pretest: React.FC<PretestProps> = ({ id, questions }) => {
+interface VerifyCodeProps extends PretestProps {
+  score: number
+  code: string
+  setTestCompleted?: (value: boolean) => void
+}
+
+const Pretest: React.FC<PretestProps> = ({ id, questions, setIsTestCompleted, courseId }) => {
   const [confirmed, setConfirmed] = useState<boolean>(false)
   const [loading, setLoading] = useState<boolean>(false)
   const [currentQuestion, setCurrentQuestion] = useState<number>(0)
+  const [score, setScore] = useState<number>(0)
+  const [code, setCode] = useState<string>('')
+  const { data: session } = useSession()
+  const [testCompleted, setTestCompleted] = useState<boolean>(false)
 
 
-  useEffect(() => {
-
-  }, [confirmed])
+  const handleSubmit = async () => {
+    setIsTestCompleted && setIsTestCompleted(true)
+    setLoading(true)
+    setConfirmed(false)
+    try {
+      if (courseId) {
+        if (score < 7) throw new Error('Failed to submit test')
+        const res = await axios.post('/api/submit-test', { cid: session?.user.cid, courseId: courseId, }, { headers: { 'Content-Type': 'application/json' } })
+        if (res.status !== 200) throw new Error('Failed to submit test')
+        if (res.data.code) setCode(res.data.code)
+        setTestCompleted(true)
+      } else {
+        throw new Error('Course ID not found')
+      }
+      setLoading(false)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <>
@@ -52,15 +83,33 @@ const Pretest: React.FC<PretestProps> = ({ id, questions }) => {
               </div>
               <div className='flex flex-col items-center justify-center w-full h-96 gap-7 text-neutral'>
                 <p>Question</p>
-                <PretestQuestion id={id} questions={questions} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion} />
+                <PretestQuestion id={id} questions={questions} currentQuestion={currentQuestion} setCurrentQuestion={setCurrentQuestion} setLoading={setLoading} score={score} setScore={setScore} />
               </div>
             </div>
-          ) : (
-            <>
-              <ConfirmPretest id={id} setConfirmed={setConfirmed} setLoading={setLoading} />
-            </>
-          )
+          ) :
+            testCompleted ? (
+              <>
+                <VerifyCode id={id} score={score} code={code} />
+              </>
+            ) :
+              (
+                <>
+                  <ConfirmPretest id={id} setConfirmed={setConfirmed} setLoading={setLoading} />
+                </>
+              )
       }
+      <dialog id={`confirm_answer_modal__${id}`} className='modal'>
+        <div className='modal-box'>
+          <h3 className='text-lg font-bold'>Confirmation</h3>
+          <p className='py-4'>Are you sure want to finish this test? (Any answer cannot be edited)</p>
+          <div className='modal-action'>
+            <form method='dialog' className='flex gap-4'>
+              <button onClick={handleSubmit} className='btn btn-primary'>Proceed</button>
+              <button className='btn btn-danger'>Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   )
 }
@@ -69,7 +118,7 @@ const ConfirmPretest: React.FC<ConfirmPretestProps> = ({ id, setConfirmed, setLo
   const { data: session } = useSession()
 
   const handleConfirm = () => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
       setConfirmed(true)
       setLoading(false)
@@ -108,13 +157,12 @@ const ConfirmPretest: React.FC<ConfirmPretestProps> = ({ id, setConfirmed, setLo
   )
 }
 
-const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQuestion, setCurrentQuestion }) => {
-  const [score, setScore] = useState<number>(0)
+const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQuestion, setCurrentQuestion, setLoading, id, score, setScore }) => {
+  const [answeredQuestions, setAnsweredQuestions] = useState<number>(0)
   const [selectedAnswers, setSelectedAnswers] = useState<string[]>(Array(questions?.length).fill(''))
   const [isTestCompleted, setIsTestCompleted] = useState<boolean>(false)
   const [randomQuestions, setRandomQuestions] = useState<Question[]>([])
-
-  console.log(randomQuestions);
+  const [answers, setAnswers] = useState<string[]>([])
 
 
   const shuffleArray = (arr: any[]) => {
@@ -125,6 +173,7 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
     const newAnswers = [...selectedAnswers]
     newAnswers[index] = answer
     setSelectedAnswers(newAnswers)
+    setAnswers(newAnswers)
 
     if (answer === randomQuestions[currentQuestion].answer) {
       if (selectedAnswers[currentQuestion] !== randomQuestions[currentQuestion].answer) {
@@ -136,9 +185,8 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
       }
     }
 
-    if (currentQuestion + 1 < randomQuestions.length) {
-      setCurrentQuestion((prev) => prev + 1)
-    }
+    const answeredCount = newAnswers.filter((answer) => answer !== '').length
+    setAnsweredQuestions(answeredCount)
   }
 
   const handleNextQuestion = () => {
@@ -153,6 +201,14 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
     }
   }
 
+  const handleFinish = () => {
+    (document.getElementById(`confirm_answer_modal__${id}`) as HTMLDialogElement)?.showModal();
+    if (isTestCompleted) {
+      setLoading(true);
+      (document.getElementById(`confirm_answer_modal__${id}`) as HTMLDialogElement)?.close();
+    }
+  }
+
   useEffect(() => {
     if (questions) {
       const shuffledQuestion = shuffleArray([...questions])
@@ -163,6 +219,7 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
       setRandomQuestions(shuffledQuestionChoices)
     }
   }, [questions])
+
   return (
     <div>
       <div>
@@ -175,9 +232,17 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
         <div className='flex gap-8'>
           {
             randomQuestions.length > 0 &&
-            randomQuestions[currentQuestion].choices.map((choice, idx) => (
-              <button key={idx} onClick={() => handleAnswer(choice, currentQuestion)} className='shadow rounded-xl w-72 btn'>{choice}</button>
-            ))
+            randomQuestions[currentQuestion].choices.map((choice, idx) => {
+              const tempAnswer = randomQuestions[currentQuestion].answer
+              if (randomQuestions[currentQuestion].choices.some((choice) => choice === tempAnswer)) {
+                return (
+                  <button key={idx} onClick={() => handleAnswer(choice, currentQuestion)} className={`shadow rounded-xl w-64 btn ${selectedAnswers[currentQuestion] === choice ? 'btn-success' : ''}`}>{choice}</button>
+                )
+              }
+              return (
+                <button key={idx} onClick={() => handleAnswer(choice, currentQuestion)} className={`shadow rounded-xl w-64 btn ${selectedAnswers[currentQuestion] === choice ? 'btn-primary' : ''}`}>{choice}</button>
+              )
+            })
           }
         </div>
         <div className='flex items-center justify-center w-full gap-8 mt-12'>
@@ -189,17 +254,60 @@ const PretestQuestion: React.FC<PretestQuestionProps> = ({ questions, currentQue
           }
           {
             currentQuestion + 1 === randomQuestions.length ?
-              <button onClick={handleNextQuestion} className='w-24 shadow cursor-not-allowed btn btn-primary' disabled>Next</button>
+              <button className='w-24 shadow cursor-not-allowed btn btn-primary' disabled>Next</button>
               :
               <button onClick={handleNextQuestion} className='w-24 shadow btn btn-primary'>Next</button>
           }
           {
-            currentQuestion + 1 === randomQuestions.length ?
-              <button onClick={() => setIsTestCompleted(true)} className='shadow w-36 btn btn-success'>Finish</button>
+            answeredQuestions === randomQuestions.length ?
+              <button onClick={handleFinish} className='shadow w-36 btn btn-success'>Finish</button>
               :
-              <button onClick={() => setIsTestCompleted(true)} className='shadow cursor-not-allowed w-36 btn btn-success' disabled>Finish</button>
+              <button className='shadow cursor-not-allowed w-36 btn btn-success' disabled>Finish</button>
           }
         </div>
+      </div>
+    </div>
+  )
+}
+
+const VerifyCode: React.FC<VerifyCodeProps> = ({ id, score, code }) => {
+  const [copied, setCopied] = useState<boolean>(false)
+  const handleCopy = () => {
+    navigator.clipboard.writeText(code)
+    setCopied(true)
+  }
+
+  return (
+    <div>
+      <h1 className='text-xl font-semibold text-neutral'>Score</h1>
+      <div className='flex flex-col items-center justify-center w-full h-96 gap-7 text-neutral'>
+        {
+          score > 6 ? (
+            <>
+              <p className='text-2xl font-bold'>Congratulations, you passed the pre-test exam!</p>
+              <h1 className='text-3xl font-bold'>Score: {score}/{10}</h1>
+              <p>Here is the verify code</p>
+              <label htmlFor="code" className='relative flex items-center'>
+                <input type='text' readOnly value={code} name='code' className='h-12 shadow pointer-events-none w-[400px] input grow input-lg text-neutral-content' />
+                {
+                  copied ?
+                    <button className='absolute w-20 right-4 btn btn-xs btn-success'>&#10003; Copied</button>
+                    :
+                    <button onClick={handleCopy} className='absolute w-20 right-4 btn btn-xs btn-primary'>Copy</button>
+                }
+              </label>
+            </>
+          )
+            :
+            (
+              <>
+                <p className='text-2xl font-bold'>You failed the pre-test exam, keep going!</p>
+                <h1 className='text-3xl font-bold'>Score: {score}/{10}</h1>
+              </>
+            )
+        }
+
+        {/* <button className='btn'>Submit</button> */}
       </div>
     </div>
   )
